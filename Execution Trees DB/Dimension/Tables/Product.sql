62100dddfb030d8faafb160e96e0464ef2a6780b
Ricks <pricardo03@hotmail.com>
2015-04-05 17:36:13 -0600

    Add DB Project

---
+++ Execution Trees DB/Dimension/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Dim].[Productos]
+(
+	 ProductId INT NOT NULL PRIMARY KEY IDENTITY(1, 1)
+	,ProductCode INT NOT NULL
+	,ProductDescription VARCHAR(100) NOT NULL
+	,ModelName VARCHAR(50) NOT NULL
+	,ProductPhoto VARBINARY(MAX) NOT NULL
+	
+)
