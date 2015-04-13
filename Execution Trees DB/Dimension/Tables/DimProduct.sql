f0b5868b9697b57cf5b54b8f893adde691d7a5aa
Ricks <pricardo03@hotmail.com>
2015-04-12 22:17:09 -0600

    primeros ejemplos

---
+++ Execution Trees DB/Dimension/Tables/DimProduct.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Sales.DimProductos
+(
+	 ProductId			INT NOT NULL PRIMARY KEY IDENTITY(1, 1)		
+	,ProductNumber		VARCHAR(25) NOT NULL
+	,ProductDescription	VARCHAR(100) NOT NULL
+	,ModelName			VARCHAR(50) NULL
+	,ProductPhoto		VARBINARY(MAX) NULL
+	,CategoryCode		VARCHAR(20) NULL
+	,CategoryName		VARCHAR(50) NULL
+	,SubCategoryCode	VARCHAR(25) NULL
+	,SubCategoryName	VARCHAR(50) NULL
+	,DimStart			DATE NOT NULL
+	,DimEnd				DATE NULL
+	,LoadGuid			UNIQUEIDENTIFIER NOT NULL
+)
--- Execution Trees DB/Dimension/Tables/ProductCategories.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [Dim].[ProductCategories]
-(	
-	 ProductCategoryCode INT NOT NULL PRIMARY KEY IDENTITY(1, 1)
-	,ProductCategoryDescription VARCHAR(50) NOT NULL
-	,ProductSubCategoryCode INT NOT NULL
-	,ProductSubCategoryName VARCHAR(50) NOT NULL
-)
